ba5892cf8a1a3df96cf6a60fe71dd971515ed095
Dattu Patel <dattu.patel@gmail.com>
2025-03-30 20:58:29 -0400

    auto add db

--- src/components/demo/index.tsx
+++ src/components/demo/index.tsx
@@ -1,6 +1,9 @@
 import { DemoSchema } from "#/db/demo";
 
 const DemoComponent = async () => {
+  await DemoSchema.create({
+    todo: 'HELLO WORLD Auto TODO'
+  })
   const demoResults = await DemoSchema.find();
   return (
     <div>
